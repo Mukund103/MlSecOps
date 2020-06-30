d9db3ce3a6989d3785ea402a2a0e06105d11a9bb
Mukund Kumar Choudhary <9430mk@gmail.com>
2020-06-30 16:04:25 +0530

    Update ipblock.py

--- ipblock.py
+++ ipblock.py
@@ -1,5 +1,5 @@
 from os import system
-with open("blockedIp.txt","r") as file1:
+with open("/mlops/blockedIp.txt","r") as file1:
   for i in file1:
     system('iptables -A INPUT -s {} -j DROP'.format(i.rstrip('\n')))
     system('service iptables save')
